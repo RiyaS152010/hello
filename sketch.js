f1e9ac507fa5a608674ace6b5b6517c527ee1dca
RiyaS152010 <125546079+RiyaS152010@users.noreply.github.com>
2023-02-16 19:15:37 +1100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var ball
+function setup() {
+  createCanvas(400,400);
+  ball= createSprite (200,200,20,20)
+}
+
+
+function draw() 
+{
+  background(30);
+if (keyDown("up"))
+{ball.x=ball.x+0
+ball.y=ball.y-2}
+drawSprites ()
+}
+
+
+
+
+
